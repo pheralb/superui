b7bb0ccab34e43fd1ad4b9fde58176cccacb4454
srdrabx <devalladolidoficial@gmail.com>
2022-08-12 21:05:12 +0200

    feat ✨: (ui) Add Button variants

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,9 +2,16 @@ import { Button } from "superui";
 
 export default function Docs() {
   return (
-    <div>
+    <div className="mx-10 my-4">
       <h1>Docs</h1>
-      <Button>Test Button</Button>
+      <div className="flex flex-col gap-3">
+        <Button variant="primary">Primary Button</Button>
+        <Button variant="secondary">Secondary Button</Button>
+        <Button variant="danger">Danger Button</Button>
+        <Button variant="primary" className="bg-gray-600">
+          Custom Button
+        </Button>
+      </div>
     </div>
   );
 }
