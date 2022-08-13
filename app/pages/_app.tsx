8b0021e8416c09a194d66a25e8354780d6fc2399
srdrabx <devalladolidoficial@gmail.com>
2022-08-13 18:48:37 +0200

    fix 🐛: (app) Add Tailwind script in dev enviroments

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
 import type { AppProps } from "next/app";
 
 // Custom styles ->
@@ -9,10 +10,16 @@ import docsTheme from "@/styles/docsTheme";
 
 // Prism syntax highlighting ->
 import "@/styles/prism.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={docsTheme}>
+      <Head>
+        {process.env.NODE_ENV === "development" && (
+          <script src="https://cdn.tailwindcss.com" defer />
+        )}
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
