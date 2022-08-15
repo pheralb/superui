a9160d583911dc5d45c787a39932d48e46bbc8bc
pheralb <pablohdez4340@gmail.com>
2022-08-15 14:42:21 +0100

    ✅ Add responsive header.

---
+++ app/components/header/links.ts
@@ -0,0 +1,17 @@
+export const HeaderLinks = [
+  {
+    title: "Community",
+    slug: "/community",
+    external: false,
+  },
+  {
+    title: "Docs",
+    slug: "/docs",
+    external: false,
+  },
+  {
+    title: "Github",
+    slug: "https://github.com/pheralb/superui",
+    external: true,
+  },
+];
