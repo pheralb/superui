a550d754d5bde70566795cdabfe572cf0f5af0ec
nachoaldamav <sr.drabx@gmail.com>
2022-08-19 11:39:46 +0200

    refactor 🔨: (ui) Remove unused functions in CMDK

--- packages/ui/CommandMenu/index.tsx
+++ packages/ui/CommandMenu/index.tsx
@@ -2,14 +2,6 @@ import { useEffect, useState } from "react";
 import { CommandMenuProps } from "./cmdk.types";
 import { Command } from "cmdk";
 import * as Dialog from "@radix-ui/react-dialog";
-import {
-  ContactIcon,
-  DocsIcon,
-  FeedbackIcon,
-  PlusIcon,
-  ProjectsIcon,
-  TeamsIcon,
-} from "./Icons";
 
 export const CommandMenu = (props: CommandMenuProps) => {
   const [open, setOpen] = useState(false);
@@ -72,47 +64,6 @@ export const CommandMenu = (props: CommandMenuProps) => {
     </Dialog.Root>
   );
 };
-
-function Components({ searchComponents }: { searchComponents: () => void }) {
-  return (
-    <>
-      <Command.Group heading="Projects">
-        <Item shortcut="S P">
-          <ProjectsIcon />
-          Search Projects...
-        </Item>
-        <Item>
-          <PlusIcon />
-          Create New Project...
-        </Item>
-      </Command.Group>
-      <Command.Group heading="Teams">
-        <Item shortcut="⇧ P">
-          <TeamsIcon />
-          Search Teams...
-        </Item>
-        <Item>
-          <PlusIcon />
-          Create New Team...
-        </Item>
-      </Command.Group>
-      <Command.Group heading="Help">
-        <Item shortcut="⇧ D">
-          <DocsIcon />
-          Search Docs...
-        </Item>
-        <Item>
-          <FeedbackIcon />
-          Send Feedback...
-        </Item>
-        <Item>
-          <ContactIcon />
-          Contact Support
-        </Item>
-      </Command.Group>
-    </>
-  );
-}
 
 function Item({
   children,
