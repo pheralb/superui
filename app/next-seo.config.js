a8d04d57abebfab5f10d20d10b63469425f14426
Juan Rojas <30534965+Davidr2998@users.noreply.github.com>
2022-08-21 12:30:13 -0500

    add(SEO): added SEO tags implementation (#109)

---
+++ app/next-seo.config.js
@@ -0,0 +1,22 @@
+const SEO = {
+  canonical: "https://superui.vercel.app/",
+  openGraph: {
+    type: "website",
+    locale: "en_us",
+    url: "https://superui.vercel.app/",
+    title: "SuperUI",
+    description: "A Tailwindcss component library",
+    images: [
+      {
+        url: "",
+        width: 1280,
+        height: 720,
+        alt: "SuperUI Banner",
+        type: "image/png",
+      },
+    ],
+  },
+  robotsProps: {},
+};
+
+export default SEO;
